96cafc56220fda8d0ea5d9c4aefcfaf3e78b2666
LanaPal <64497295+LanaPal@users.noreply.github.com>
2020-10-15 20:57:58 -0500

    draft app file

--- app.py
+++ app.py
@@ -1,14 +1,56 @@
-from flask import Flask, render_template, request, jsonify, redirect
+import os
+
+from flask import Flask, render_template, jsonify, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+# from flask import send_from_directory
+from gevent.pywsgi import WSGIServer
+from yourapplication import app
+http_server = WSGIServer(('', 5000), app)
+http_server.serve_forever()
+
+import tensorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+f
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 
 ######## INITIATE FLASK APP #########################
 app= Flask(__name__)
 
-@app.route ("/home")
-@app.route("/")
-def welcome():
-    return render_template('index.html')
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+@app.route('/', methods=['POST', 'GET'])
+def get_data():
+    if request.method == 'POST':
+        user = request.form['search']
+        get_image_class(user)
+        return redirect(url_for('success', name=get_directory(user)))
+
+
+@app.route('/success/<name>')
+def success(name):
+    return render_template('image_class.html')
+
+
 # @app.route("/Skin")
 # def graph():
 
