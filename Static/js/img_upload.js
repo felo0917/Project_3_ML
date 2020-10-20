a5e66660474e95deab8a3bb381e687db76c23cfc
Dany Koudede <koudededany@icloud.com>
2020-10-20 13:23:02 -0700

    PlanB
    running app

--- Static/js/img_upload.js
+++ Static/js/img_upload.js
@@ -1,25 +1,25 @@
-const inputFile = document.getElementById('inputFile');
-const prevContainer = document.getElementById('imagePreview');
-const prevImg = prevContainer.querySelector('.img_prev_img');
-const prevDeftext = prevContainer.querySelector('.img_prev_text');
+// const inputFile = document.getElementById('inputFile');
+// const prevContainer = document.getElementById('imagePreview');
+// const prevImg = prevContainer.querySelector('.img_prev_img');
+// const prevDeftext = prevContainer.querySelector('.img_prev_text');
 
 
-inputFile.addEventListener('change', function() {
-    const file = this.files[0];
+// inputFile.addEventListener('change', function() {
+//     const file = this.files[0];
 
-    console.log(file)
+//     console.log(file)
 
-    if (file) {
-        const reader = new FileReader();
+//     if (file) {
+//         const reader = new FileReader();
 
-        prevDeftext.style.display = 'none';
-        prevImg.style.display = 'block';
+//         prevDeftext.style.display = 'none';
+//         prevImg.style.display = 'block';
 
-        reader.addEventListener('load', function() {
+//         reader.addEventListener('load', function() {
            
-            prevImg.setAttribute('src', this.result);
-        });
+//             prevImg.setAttribute('src', this.result);
+//         });
 
-        reader.readAsDataURL(file);
-    }
-});
+//         reader.readAsDataURL(file);
+//     }
+// });
