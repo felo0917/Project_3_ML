dd8182e29f1d8d32a22144439f9154d085506063
Dany Koudede <koudededany@icloud.com>
2020-10-22 19:40:14 -0700

    App work
    working on app prediction (front_end)
    changing prediction function result
    fixed js issue

--- app.py
+++ app.py
@@ -1,67 +1,67 @@
-  
-from __future__ import division, print_function
-# coding=utf-8
-# import sys
-# import os
-# import glob
-# import re
-# import numpy as np
-# import tensorflow
-# from tensorflow import keras
+import os
+from uuid import uuid4
 
-# Keras
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from keras.models import load_model
-from keras.preprocessing import image
+from flask import Flask, request, render_template, send_from_directory
 
-# Flask utils
-from flask import Flask, redirect, url_for, request, render_template
-from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
 
-# Define a flask app
 app = Flask(__name__)
+# app = Flask(__name__, static_folder="images")
 
-# MODEL_PATH = r'Static\model\skin_train.h5'
 
-# model = load_model(MODEL_PATH)
-# model.make_predict_function()
-# model.summary()
-# def model_predict(img_path, model):
-#     img = image.load_img(img_path, target_size=(224, 224))
 
-#     #preprocessing the image
-#     x = image.img_to_array(img)
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-#     x = np.expand_dims(x, axis=0)
+classes = ['Eczema', 'Vascular Tumors', 'Bullous Disease', 'Nail Fungus']
 
-#     x = preprocess_input(x, mode='caffe')
 
-#     preds = model.predict(x)
-#     return preds
 
-@app.route('/', methods=['GET'])
+@app.route("/")
 def index():
-    return render_template('flask.html')
+    return render_template("index1.html")
 
-# @app.route('/predict', methods=['GET', 'POST'])
-# def upload():
-#     if request.method == 'POST':
-#         f = request.files['file']
+@app.route("/upload", methods=["POST"])
+def upload():
+    target = os.path.join(APP_ROOT, 'images/')
+    # target = os.path.join(APP_ROOT, 'static/')
+    print(target)
+    if not os.path.isdir(target):
+            os.mkdir(target)
+    else:
+        print("Couldn't create upload directory: {}".format(target))
+    print(request.files.getlist("file"))
+    for upload in request.files.getlist("file"):
+        print(upload)
+        print("{} is the file name".format(upload.filename))
+        filename = upload.filename
+        destination = "/".join([target, filename])
+        print ("Accept incoming file:", filename)
+        print ("Save it to:", destination)
+        upload.save(destination)
+        #import tensorflow as tf
+        import numpy as np
+        from keras.preprocessing import image
+        from keras.models import load_model
+       
+        new_model = load_model('Static/model/skin_train.h5')
+        new_model.summary()
+        test_image = image.load_img('images\\'+filename,target_size=(224,224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis =0)
+        result = new_model.predict(test_image)
+        result1 = result[0]
+        for i in range(3):
+    
+            if result1[i] == 1.:
+                break
+        prediction = classes[i]
 
-#         #save the file to ./uploads
-#         basepath = os.path.dirname(__file__)
-#         file_path = os.path.join(
-#             basepath, 'uploads', secure_filename(f.filename))
-#         f.save(file_path)
+    # return send_from_directory("images", filename, as_attachment=True)
+    return render_template("template.html",image_name=filename, text=prediction)
 
-#         preds = model_predict(file_path, model)
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("images", filename)
 
-#         pred_class = decode_predictions(preds, top=1)
-#         result = str(pred_class[0][0][1])
-#         return result
-#     return None
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=False)
+  
